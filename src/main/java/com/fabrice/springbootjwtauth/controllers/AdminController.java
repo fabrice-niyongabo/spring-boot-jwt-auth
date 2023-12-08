a9a2d2f38c8dedf9b1deec0415b2fbe19dd0b7e0
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-12-08 21:11:54 +0200

    created boilerplate controllers

---
+++ src/main/java/com/fabrice/springbootjwtauth/controllers/AdminController.java
@@ -0,0 +1,12 @@
+package com.fabrice.springbootjwtauth.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
+public class AdminController {
+}
