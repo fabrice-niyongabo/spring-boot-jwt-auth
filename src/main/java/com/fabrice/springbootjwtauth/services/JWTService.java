9c0cf926cd766d526bea28947533d5ef6c5be674
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-12-08 20:10:45 +0200

    configured jwt claims

---
+++ src/main/java/com/fabrice/springbootjwtauth/services/JWTService.java
@@ -0,0 +1,4 @@
+package com.fabrice.springbootjwtauth.services;
+
+public interface JWTService {
+}
---
+++ src/main/java/com/fabrice/springbootjwtauth/services/impl/JWTServiceImpl.java
@@ -0,0 +1,52 @@
+package com.fabrice.springbootjwtauth.services.impl;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JWTServiceImpl {
+
+    private  String generateToken (UserDetails userDetails){
+        return Jwts.builder().setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24)) // the token will be valid for 24hours (you can update the number/millis as you want)
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    public String extractUsername(String token){
+        return extractClaim(token,Claims::getSubject);
+    }
+
+
+    private <T> T extractClaim(String token, Function<Claims,T> claimsResolvers){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignKey() {
+        //obtain the key encryption online
+        String keySecret = "cecff5acea03c072e9a36a0faf8fa76bda39e1a2111c3086a10de378d4a96585";
+        byte[] key = Decoders.BASE64.decode(keySecret);
+        return Keys.hmacShaKeyFor(key);
+
+}}
