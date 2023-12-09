9b0565375b15576b11295174f18e2caf37d5d380
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-12-09 22:10:25 +0200

    not found error handler

--- src/main/java/com/fabrice/springbootjwtauth/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/fabrice/springbootjwtauth/exceptions/GlobalExceptionHandler.java
@@ -5,12 +5,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    //global exceptions => reformatting spring boot exception response
+    //handle not found exception
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND) //response status code
+    public ErrorResponse handleNotFoundException(NotFoundException exception){
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatusCode(HttpStatus.NOT_FOUND.value());
+        errorResponse.setMessage(exception.getMessage());
+        return  errorResponse;
+    }
+
+
+    // global exceptions => reformatting spring boot exception response
+    // for all remaining kind of errors which can be found within our app
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGlobalExceptions(Exception exception){
         String message  =  exception.getMessage();
